b7db7f701d7817fe8b1b8775c791c040d5af52f9
esraaalamawy <esraagamalalamawy5112000@gmail.com>
2023-03-04 15:38:14 +0200

    smart-resturent

--- smart.js
+++ smart.js
@@ -3,12 +3,14 @@ var box= document.getElementById("box")
 var show= document.getElementById("show")
 var price=document.getElementById("totalprice")
 var order = document.getElementById("order")
+var re= document.getElementById("reset")
 var total=0
 all.forEach( function(item){
     item.onclick=function(){
         box.innerHTML += item.getAttribute("name") + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"
         show.style.display="block"
         order.style.display="block"
+        re.style.display="block"
         total +=+(item.getAttribute("price"))
         price.style.display="none"
     }
@@ -18,4 +20,10 @@ all.forEach( function(item){
 show.onclick=function(){
     price.style.display="block"
     price.innerHTML="total price is: " + total + " LE"
+}
+
+re.onclick= function(){
+    total=0
+    box.innerHTML =""
+    price.style.display="none"
 }
